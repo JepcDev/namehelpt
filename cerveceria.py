6ab68d0e4f1f0898645d3e995214dd3327bb8a38
JepcDev <joseenrique.pc21@gmail.com>
2024-09-08 19:14:49 -0400

    fix: Cambiamos la frase principal y las concatenacion con espacios

--- cerveceria.py
+++ cerveceria.py
@@ -1,3 +1,3 @@
 #Proyecto
 #Generador de nombre para marca de cereveza
-print("El nombre de tu cerveza es \n" + "\"" + (input("Cual es tu color favorito en 1 palabra: ")) + " " + (input("Cual es tu fruta favorita: ")) +"\"")
+print("El nombre de tu cerveza es \n" + "\"" + (input("Que lugar en el mundo te gustaria visitar?: ")) + " " + (input("Cual es tu color favorito?: ")) +"\"")
