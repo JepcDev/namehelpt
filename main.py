8a43c33b69ffd8337d2070ae4e6acf846c4aeb7c
JepcDev <joseenrique.pc21@gmail.com>
2024-09-08 14:58:23 -0400

    Add: declaracion print

---
+++ main.py
@@ -0,0 +1,10 @@
+#Imprime lo que esta dentro de los parentesis
+print("Hello world")
+
+#Para imprimir algo entre comillas se usa el tipo diferente de comilla al que se empezo a usar al inicio para que el lenguaje lo diferencie y lo imprima
+print("Hola mundo, 'hello'")
+
+print('100 + 50')
+
+#Imprime numeros operaicones, ect sin las comillas para las demas funciones o numeros
+print(100 + 50)
