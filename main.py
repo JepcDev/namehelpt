edcf3fc03a116ab86d54a819bf3e639bf72178bb
JepcDev <joseenrique.pc21@gmail.com>
2024-09-08 16:02:41 -0400

    ADD: String y practica string

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 #Imprime lo que esta dentro de los parentesis
-print("Hello world")
+#print("Hello world")
 
 #Para imprimir algo entre comillas se usa el tipo diferente de comilla al que se empezo a usar al inicio para que el lenguaje lo diferencie y lo imprima
-print("Hola mundo, 'hello'")
+#print("Hola mundo, 'hello'")
 
-print('100 + 50')
+#print('100 + 50')
 
 #Imprime numeros operaicones, ect sin las comillas para las demas funciones o numeros
-print(100 + 50)
+#print(100 + 50)
+
+#String
+
+#print("Hola mundo")
+
+#print("Hola" + " " + "mundo")
+
+#\" Todo lo que se pone despues de la barra invertida se lo interpreta como un caracter especial
+#print("Me llamo \"Jose\" ")
+
+#El caracter especial \n significa salto de linea
+#print("Hola soy una linea \nHola soy otra linea")
+
+#El caracter especial \t sinifica tabulacion
+#print("\t Soy otra linea")
+
+#Caracter especial \' se imprime el apostrofe
+#print("this isn\'t a number")
+
+#Caracter especial \\ se imprime la barra invertida
+#print("Este signo \\ es una barra invertida")
+
+#Tarea
+print("Línea 1 \nLínea 2 \nLínea 3") 
