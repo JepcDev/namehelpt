41cc499a0a45a59955dfa799e91ecd665131d601
JepcDev <joseenrique.pc21@gmail.com>
2024-09-19 19:41:01 -0400

    ADD: practica de print, string, input

---
+++ practica.py
@@ -0,0 +1,21 @@
+#Practicas
+#PRINT
+print("Me encanta estudiar python")
+print("Estudiarcon \"Python Total\" es super divertido")
+print(5 + 555)
+
+#STRING
+print("Linea1\nLinea2\nLinea3")
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+print("Barra normal: / \nBarra invertida: \\")
+
+#INPUT
+respuesta = input("Que estas estudiando?: ")
+print(respuesta)
+
+pais_usuario = input("En que pais vives?: ")
+print(pais_usuario)
+
+nombre_usuario = input("Escribe tu nombre: ")
+apellido_usuario = input("Escribe tu apellido: ")
+print(nombre_usuario + " " + apellido_usuario)
